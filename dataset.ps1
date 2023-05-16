f7c0cedb87eee8823f546d56ed42f5644f2eb408
hicoldcat <hicoldcat@foxmail.com>
2023-05-17 00:35:18 +0800

    Test for preprocess

---
+++ dataset.ps1
@@ -0,0 +1,18 @@
+
+# [CmdletBinding()]
+# Param (
+#   [String]$LocalHost,
+#   [Int32]$LocalPort
+# )
+
+.\venv\Scripts\activate
+
+# pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 -f https://mirror.sjtu.edu.cn/pytorch-wheels/torch_stable.html -i https://mirrors.bfsu.edu.cn/pypi/web/simple
+# pip install -U -I --no-deps xformers==0.0.19 -i https://mirrors.bfsu.edu.cn/pypi/web/simple
+# pip install --upgrade -r .\train_tools\requirements.txt -i https://mirrors.bfsu.edu.cn/pypi/web/simple
+# pip install --upgrade -r .\train_tools\repositories\BLIP\requirements.txt -i https://mirrors.bfsu.edu.cn/pypi/web/simple
+# pip install --upgrade -r .\train_tools\repositories\CodeFormer\requirements.txt -i https://mirrors.bfsu.edu.cn/pypi/web/simple
+# pip install --upgrade -r .\train_tools\repositories\k-diffusion\requirements.txt -i https://mirrors.bfsu.edu.cn/pypi/web/simple
+# pip install --upgrade -r .\train_tools\repositories\stable-diffusion-stability-ai\requirements.txt -i https://mirrors.bfsu.edu.cn/pypi/web/simple
+
+python .\train_tools\dataset.py
