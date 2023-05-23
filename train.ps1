d68a011aac2879ca7a41e40ac7cb7f9352dc16e8
hicoldcat <hicoldcat@foxmail.com>
2023-05-23 21:01:10 +0800

    update

--- train.ps1
+++ train.ps1
@@ -3,9 +3,11 @@
 Param (
   [Parameter(Mandatory = $true)]
   [string]$config_file,
-  [int]$num_cpu_threads_per_process = 8,
+  [int]$num_cpu_threads_per_process = 1,
   [int]$utf8 = 1
 )
+
+$pythonProcess = $null
 
 .\venv\Scripts\activate
 
@@ -15,5 +17,10 @@ if ($utf8 -eq 1) {
   $Env:PYTHONUTF8 = 1
 }
 
-python -m accelerate.commands.launch --num_cpu_threads_per_process=$num_cpu_threads_per_process "./sd-scripts/train_network.py" `
-  --config_file=$config_file
+# python -m accelerate.commands.launch --num_cpu_threads_per_process=$num_cpu_threads_per_process "./sd-scripts/train_network.py" --config_file=$config_file
+
+$pythonProcess = Start-Process -FilePath python -ArgumentList "-m", "accelerate.commands.launch", "--num_cpu_threads_per_process=$num_cpu_threads_per_process", "./sd-scripts/train_network.py", "--config_file=$config_file" -NoNewWindow -PassThru 
+
+$pythonPid = $pythonProcess.Id
+
+Write-Host "Python-Process-ID=$pythonPid"
