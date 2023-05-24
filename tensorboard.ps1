a5152064ce253602d56efc162dc79770bb0dd4bd
hicoldcat <hicoldcat@foxmail.com>
2023-05-24 13:43:49 +0800

    Update tensorboard.ps1

--- tensorboard.ps1
+++ tensorboard.ps1
@@ -10,5 +10,12 @@ $Env:TF_CPP_MIN_LOG_LEVEL = "3"
 
 .\venv\Scripts\activate
 
-python -m tensorboard.main --logdir=$logdir --port=$port
+# python -m tensorboard.main --logdir=$logdir --port=$port
 # python .\diagnose_tensorboard.py
+
+
+$pythonProcess = Start-Process -FilePath python -ArgumentList "-m", "tensorboard.main", "--logdir=$logdir", "--port=$port" -NoNewWindow -PassThru 
+
+$pythonPid = $pythonProcess.Id
+
+Write-Host "Python-Process-ID=$pythonPid"
