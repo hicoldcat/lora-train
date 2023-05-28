d7357c996755591cbbfe363259e48d20ad841d99
hicoldcat <hicoldcat@foxmail.com>
2023-05-28 19:11:10 +0800

    mac cuda is unavilable

--- setup.bash
+++ setup.bash
@@ -1,44 +1,55 @@
-echo "正在创建虚拟环境..."
+# mac cuda 暂不可用
 
-python3 -m venv venv
+# #!/usr/bin/bash
+# current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-source venv/bin/activate
+# if [ -d "venv" ]; then
+#   echo "venv folder exists."
+# else
+#   python3 -m venv venv
+#   echo "正在创建虚拟环境..."
+# fi
 
-$cn_source = $1
+# source "$current_dir/venv/bin/activate"
 
-# cd ./sd-scripts
+# echo $1
 
-if [ -n "$cn_source" ]; then
-    echo "安装 torch & xformers..."
-    pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 --extra-index-url https://mirrors.bfsu.edu.cn/pypi/web/simple
-    pip install xformers==0.0.19  --extra-index-url https://mirrors.bfsu.edu.cn/pypi/web/simple
+# cn_source=$1
 
-    echo "安装依赖..."
+# echo $cn_source
+
+# if [ -n "$cn_source" ]; then
+#     echo "安装 torch & xformers..."
+#     pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 --extra-index-url https://mirrors.bfsu.edu.cn/pypi/web/simple
+#     pip install xformers==0.0.19  --extra-index-url https://mirrors.bfsu.edu.cn/pypi/web/simple
+
+#     echo "安装依赖..."
     
-    cd ./preprocess_tools
+#     cd ./preprocess_tools
 
-    pip install --upgrade -r requirements.txt --extra-index-url https://mirrors.bfsu.edu.cn/pypi/web/simple
+#     pip install --upgrade -r requirements.txt --extra-index-url https://mirrors.bfsu.edu.cn/pypi/web/simple
 
-    cd ../sd-scripts
+#     cd ../sd-scripts
 
-    pip install --upgrade -r requirements.txt  --extra-index-url https://mirrors.bfsu.edu.cn/pypi/web/simple
-    pip install --upgrade lion-pytorch lycoris-lora dadaptation wandb  --extra-index-url https://mirrors.bfsu.edu.cn/pypi/web/simple
-else
-    echo "安装 torch & xformers..."
-    pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
-    pip install xformers==0.0.19
+#     pip install --upgrade -r requirements.txt  --extra-index-url https://mirrors.bfsu.edu.cn/pypi/web/simple
+#     pip install --upgrade lion-pytorch lycoris-lora dadaptation wandb  --extra-index-url https://mirrors.bfsu.edu.cn/pypi/web/simple
+# else
+#     echo "安装 torch & xformers..."
+#     pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
+#     pip install xformers==0.0.19
 
-    echo "安装依赖..."
+#     echo "安装依赖..."
 
-    cd ./preprocess_tools
-    pip install --upgrade -r requirements.txt
+#     cd ./preprocess_tools
+#     pip install --upgrade -r requirements.txt
 
-    cd ../sd-scripts
+#     cd ../sd-scripts
 
-    pip install --upgrade -r requirements.txt
-    pip install --upgrade lion-pytorch lycoris-lora dadaptation wandb
+#     pip install --upgrade -r requirements.txt
+#     pip install --upgrade lion-pytorch lycoris-lora dadaptation wandb
 
-    
-echo "安装完毕"
+# fi
 
-exit
+# echo "安装完毕"
+
+# exit
